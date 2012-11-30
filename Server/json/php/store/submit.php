34d38864119c2ffb172815d5fee2e0a71ac5a6f7
Chad Sellers <chad@usefulfruit.com>
2012-11-30 14:59:00 -0500

    Update example php server to split out fields and save them to files, as well as un-base64 encode the documents uploaded (but not unzip them).

--- Server/json/php/store/submit.php
+++ Server/json/php/store/submit.php
@@ -39,6 +39,18 @@ function uniq()
     return date('Y-m-d\\TH:i:s-') . md5(getmypid().uniqid(rand()).$_SERVER[‘SERVER_NAME’]);
 }
 
+function write_to_file($data,$filename)
+{
+   $fh = fopen($filename, "w");
+   if (!$fh) {
+      // failed to create file
+      echo "ERR 004\n";
+      echo "failed to create file" . $filename;
+   }
+   fwrite($fh, $data);
+   fclose($fh);
+}
+
 $project_raw = $_GET['project'];
 $project = preg_replace('/[^(0-9A-Za-z)]*/', '', $project_raw);
 
@@ -86,15 +98,27 @@ if (!mkdir($submission_dir)) {
 }
 
 $postdata = file_get_contents("php://input");
-$dest = $submission_dir . '/body';
-$fh = fopen($dest, "w");
-if (!$fh) {
-   // failed to create file
-   echo "ERR 004 $file\n";
-   echo "failed to create file";
-}
-fwrite($fh, $postdata);
-fclose($fh);
+$dest = $submission_dir . '/raw_json';
+write_to_file($postdata,$dest);
 
+$json_data = json_decode($postdata);
+foreach ($json_data as $key => $value) {
+   $dest = $submission_dir . $key;
+   if ($key == 'documents') {
+      if (mkdir($dest)) {
+         foreach ($value as $docname => $docdata) {
+            $doc_dest = $dest . '/' . $docname;
+            $decoded_data = base64_decode($docdata);
+            write_to_file($decoded_data,$doc_dest);
+         }
+      }
+      else {
+         echo "failed to create direcory for documents";
+      }
+   }
+   else {
+      write_to_file($value,$dest);
+   }
+}
 
 ?>
