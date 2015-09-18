25215e9e5619a6c46be5038f5a106539eb3c63a9
Brent Lord <brent@figure53.com>
2015-09-18 17:30:23 -0400

    add iOS framework target

---
+++ F53FeedbackKit_iOS/F53FeedbackKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  F53FeedbackKit_iOS.h
+//  F53FeedbackKit_iOS
+//
+//  Created by Brent Lord on 9/18/15.
+//  Copyright © 2015 Figure 53, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for F53FeedbackKit_iOS.
+FOUNDATION_EXPORT double F53FeedbackKit_iOSVersionNumber;
+
+//! Project version string for F53FeedbackKit_iOS.
+FOUNDATION_EXPORT const unsigned char F53FeedbackKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F53FeedbackKit_iOS/PublicHeader.h>
+
+
